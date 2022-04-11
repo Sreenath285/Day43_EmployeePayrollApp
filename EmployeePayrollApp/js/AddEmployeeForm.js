5d7148b49468dda76c675a2a17a62bc09cd7470a
Sreenath285 <70939307+Sreenath285@users.noreply.github.com>
2022-04-11 16:04:43 +0530

    Update AddEmployeeForm.js

--- EmployeePayrollApp/js/AddEmployeeForm.js
+++ EmployeePayrollApp/js/AddEmployeeForm.js
@@ -21,21 +21,3 @@ window.addEventListener("DOMContentLoaded", (event) => {
         output.textContent = salary.value;
   });
 });
-
-// const nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
-// const text = document.querySelector("#name");
-// const textError = document.querySelector(".text-error");
-// text.addEventListener("input", function () {
-//   if (nameRegex.test(text.value)) {
-//     textError.textContent = "";
-//   } else {
-//     textError.textContent = "Incorrect name";
-//   }
-// });
-
-// const salary = document.querySelector("#salary");
-// const output = document.querySelector(".salary-output");
-// output.textContent = salary.value;
-// salary.addEventListener("input", function () {
-//   output.textContent = salary.value;
-// });
